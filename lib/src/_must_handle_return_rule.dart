26ff6d4327126d7e5db827cd70a594c85b556045
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-15 17:38:48 -0400

    update

--- lib/src/_must_handle_return_rule.dart
+++ lib/src/_must_handle_return_rule.dart
@@ -33,10 +33,8 @@ class MustHandleReturnRule extends DartLintRule {
 
   static const _code = LintCode(
     name: 'must_handle_return',
-    problemMessage:
-        'The return value of a function annotated with @mustHandleReturn must be used.',
-    correctionMessage:
-        'Assign the result to a variable, or use it in an expression.',
+    problemMessage: 'The return value of a function annotated with @mustHandleReturn must be used.',
+    correctionMessage: 'Assign the result to a variable, or use it in an expression.',
     errorSeverity: ErrorSeverity.WARNING,
   );
 
@@ -129,16 +127,15 @@ class MustHandleReturnRule extends DartLintRule {
 
   bool _isResultUnused(AstNode node) {
     // For identifiers, we need to check the parent to see if it's an invocation.
-    final nodeToCheck = node.parent is InvocationExpression
-        ? node.parent!
-        : node;
+    final nodeToCheck = node.parent is InvocationExpression ? node.parent! : node;
 
     var parent = nodeToCheck.parent;
     if (parent is CascadeExpression) return true;
     while (parent != null) {
       if (parent is ExpressionStatement) return true;
-      if (parent is! ParenthesizedExpression && parent is! AwaitExpression)
+      if (parent is! ParenthesizedExpression && parent is! AwaitExpression) {
         return false;
+      }
       parent = parent.parent;
     }
     return false;
