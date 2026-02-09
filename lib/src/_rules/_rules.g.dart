64a54766318c47fa57524c6d85d68d4b5df453b9
t0mb3rr <robmllze@gmail.com>
2026-02-09 22:30:23 +1000

    +Minor updates

--- lib/src/_rules/_rules.g.dart
+++ lib/src/_rules/_rules.g.dart
@@ -7,11 +7,11 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
+export './must_await_all_futures_rule.dart';
+export './must_be_anonymous_rule.dart';
 export './must_be_strong_ref_rule.dart';
+export './must_handle_return_rule.dart';
+export './must_use_outcome_rule.dart';
 export './must_use_unsafe_wrapper_rule.dart';
-export 'must_use_outcome_rule.dart';
 export './no_future_monad_rule.dart';
 export './no_futures_rule.dart';
-export './must_be_anonymous_rule.dart';
-export './must_handle_return_rule.dart';
-export './must_await_all_futures_rule.dart';
